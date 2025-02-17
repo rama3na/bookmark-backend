b28a4c9a61ea8bab5cc1943fe178360cbe8f6697
rama3na <dyavarivenkat2662@gmail.com>
2025-02-17 11:18:14 +0530

    Initial commit

--- APIs/AuthApi.js
+++ APIs/AuthApi.js
@@ -1,6 +1,6 @@
 const exp = require("express");
 const expressAsyncHandler = require("express-async-handler");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const authApp = exp.Router();
